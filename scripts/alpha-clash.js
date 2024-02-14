cb324914496181c08b8f9afdd6d9ad6dd118d28e
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-02-14 23:23:15 +0600

    some js fuction added for keyup method

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -10,6 +10,25 @@
     playGroundSection.classList.remove('hidden');
 } */
 
+function handleKeyboardButtonPress(event) {
+    const playerPressed = event.key;
+    console.log('player pressed :', playerPressed);
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, expectedAlphabet);
+
+    // check matched or not
+    if (playerPressed === expectedAlphabet) {
+        console.log('you got a point');
+    } else {
+        console.log('you missed. you lost a life');
+    }
+}
+document.addEventListener('keyup', handleKeyboardButtonPress)
+// capture keyboard key press
 function continueGame() {
     // step-1: generate a random alphabet
     const alphabet = getARandomAlphabet();
