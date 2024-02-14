e8a8807687ffad379829fa364aae3440bb5e17b3
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-02-15 01:23:32 +0600

    added js code shorter than privious js code in section playground

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -21,34 +21,51 @@ function handleKeyboardButtonPress(event) {
     // check right or wrong key pressed
     if (playerPressed === expectedAlphabet) {
         console.log('you got a point');
-        console.log('you have pressed correctly', expectedAlphabet);
-        // update score:
-        // 1. get the current score
-        const currentScoreElement = document.getElementById('current-score');
-        const currentScoreText = currentScoreElement.innerText;
-        console.log(currentScoreText);
-        const currentScore = parseInt(currentScoreText);
-        console.log(currentScore);
 
-        // 2. increase the score by 1
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+
+
+
+        // --------------------
+        // console.log('you have pressed correctly', expectedAlphabet);
+        // // update score:
+        // // 1. get the current score
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // console.log(currentScoreText);
+        // const currentScore = parseInt(currentScoreText);
+        // console.log(currentScore);
+        /* const currentScore = getTextElementValueById('current-score'); */
+
+        // // 2. increase the score by 1
         const newScore = currentScore + 1;
-        // 3. show the updated score
-        currentScoreElement.innerText = newScore;
+        // // 3. show the updated score
+        // currentScoreElement.innerText = newScore;
+
         // start a new round
         removeBackgroundColorById(expectedAlphabet);
         continueGame();
     } else {
         console.log('dhurrr vaiya va apu... right key press koro');
-        // step-1: get the current Life number
-        const currentLifeElement = document.getElementById('current-life');
-        const currentLifeText = currentLifeElement.innerText;
-        const currentLife = parseInt(currentLifeText);
 
-        // step-2: reduce the life count
-        const newLife = currentLife - 1;
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
 
-        // step-3: display the updated life count
-        currentLifeElement.innerText = newLife;
+
+        // -------------------------
+        // // step-1: get the current Life number
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+
+        // // step-2: reduce the life count
+        // const newLife = currentLife - 1;
+
+        // // step-3: display the updated life count
+        // currentLifeElement.innerText = newLife;
     }
     /* const playerPressed = event.key;
     console.log('player pressed :', playerPressed);
